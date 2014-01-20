e412ba30efd805688e2a8b624af0fe6105924918
Nigel Gott <nigel.gott@gmail.com>
2014-01-20 12:09:05 +0000

    Changed to use basic package structure.

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="DBN-Library",
+        version="1.0",
+        description="An implementation of Random Boltzman Manchines and Deep Belief Networks.",
+        packages='dbnlib'
+        )
