fe6ed1ea14188147fb55250f835d30fbb3add3e0
Nigel Gott <nigel.gott@gmail.com>
2014-01-20 14:28:02 +0000

    Added second test of dbn.number_inputs.

--- tests/test_dbn.py
+++ tests/test_dbn.py
@@ -11,4 +11,12 @@ class TestDBN(unittest.TestCase):
         d = DBN(layer_sizes, 0, 0)
         self.assertEquals(d.number_layers, 3)
 
+    def test_number_inputs_setup_correct(self):
+        # The first layer in the layer sizes corrosponds to the inputs.
+        layer_sizes = [144, 50, 50, 2000]
+        d = DBN(layer_sizes, 0, 0)
+        self.assertEquals(d.number_inputs, 144)
+
+
+
 
