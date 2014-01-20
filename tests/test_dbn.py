f0817c755ce5707d5bb610b8a22c8ea2cbe26d39
Nigel Gott <nigel.gott@gmail.com>
2014-01-20 14:46:31 +0000

    Added test for get_topology.

--- tests/test_dbn.py
+++ tests/test_dbn.py
@@ -8,19 +8,27 @@ class TestDBN(unittest.TestCase):
         # the stacked RBMs share indivdual layers, so the first RBM is 144, 50. 
         # The second is 50, 50 etc.
         layer_sizes = [144, 50, 50, 2000]
-        d = DBN(layer_sizes, 0, 0)
+        d = DBN(layer_sizes, 1)
         self.assertEquals(d.number_layers, 3)
 
     def test_number_inputs_setup_correct(self):
         # The first layer in the layer sizes corrosponds to the inputs.
         layer_sizes = [144, 50, 50, 2000]
-        d = DBN(layer_sizes, 0, 0)
+        d = DBN(layer_sizes, 1)
         self.assertEquals(d.number_inputs, 144)
 
     def test_rbms_list_setup_correct(self):
         layer_sizes = [144, 50, 50, 2000]
-        d = DBN(layer_sizes, 0, 0)
+        d = DBN(layer_sizes, 1)
         self.assertEquals(len(d._rbms), d.number_layers)
 
+    def test_get_topology_constructs(self):
+        # get_topology should return the layer_sizes with the number of labels 
+        # on the end of the list.
+        layer_sizes = [144, 50, 50, 2000]
+        number_labels = 1
+        d = DBN(layer_sizes, number_labels)
+        layer_sizes.append(number_labels)
+        self.assertListEqual(d.get_topology(), layer_sizes)
 
 
